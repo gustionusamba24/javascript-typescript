bcd69579545e49282b92563c32fd375cb8f06d3a
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-10 20:09:58 +0700

    feat(03-developer-skills-tools): finish challenge 1 on developer skills and tools

---
+++ challenges/03-developer-skills-tools/challenge1.js
@@ -0,0 +1,13 @@
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = (arr) => {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} ${i > 0 ? "days" : "day"}...`;
+  }
+  console.log(`...${str}`);
+};
+
+printForecast(data1);
+printForecast(data2);
