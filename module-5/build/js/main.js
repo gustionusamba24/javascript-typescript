79281a2bddf8ad4a820b5b1a87c09b10ad183b97
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-21 14:40:53 +0700

    module-5: type aliases

---
+++ module-5/build/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+let userID;
+let postID;
+userID = "user123";
+console.log(userID);
+postID = "post123";
+console.log(postID);
+postID = 12345;
+console.log(postID);
+const user1 = {
+    id: 1,
+    name: "Gustio",
+    isOnline: true,
+};
+console.log(user1);
+console.log(user1.name);
+const user2 = {
+    id: 2,
+    name: "Hatmaja",
+    isOnline: false,
+};
+console.log(user2);
