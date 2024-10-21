f3de12f2dc95961c31acf0c809a1fffdc3dd31f4
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-21 15:07:17 +0700

    module-6: optional property

---
+++ module-6/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let user1 = {
+    id: 1,
+    name: "Nusamba",
+    age: 24,
+    email: "gustio@gmail.com",
+};
+console.log(user1);
+let user2 = {
+    id: 2,
+    name: "Narotama",
+    isOnline: false,
+    petName: "Gugu gaga",
+    email: "narotama@gmail.com",
+};
+console.log(user2);
