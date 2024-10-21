f229b9aa78e295b10423cf1f7fe6426b49cdef83
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-21 21:57:35 +0700

    feat(module-3): using unknown instead of any

--- module-3/build/js/main.js
+++ module-3/build/js/main.js
@@ -1,4 +1,6 @@
 "use strict";
+// any data types
+console.log("========= Any Data Types =========");
 let variable;
 variable = "Samba";
 variable = 24;
@@ -13,3 +15,20 @@ anyRisk(3.1443428947892034);
 logValue("Aku cinta dia");
 logValue(3.1443428947892034);
 console.log(variable);
+// unknown data types (better than any)
+console.log("========= Unknown Data Types =========");
+let data;
+data = 123;
+data = true;
+data = "Gustio Nusamba";
+data = [true, false, "wkwk", 123];
+// check data types wheter array or not
+if (typeof data === "string") {
+    console.log(data.toUpperCase());
+}
+else if (Array.isArray(data)) {
+    console.log(data.join(", "));
+}
+else {
+    console.log("kesalahan tipe data");
+}
--- module-3/src/main.ts
+++ module-3/src/main.ts
@@ -1,3 +1,5 @@
+// any data types
+console.log("========= Any Data Types =========");
 let variable: any;
 
 variable = "Samba";
@@ -18,3 +20,21 @@ logValue("Aku cinta dia");
 logValue(3.1443428947892034);
 
 console.log(variable);
+
+// unknown data types (better than any)
+console.log("========= Unknown Data Types =========");
+let data: unknown;
+
+data = 123;
+data = true;
+data = "Gustio Nusamba";
+data = [true, false, "wkwk", 123];
+
+// check data types wheter array or not
+if (typeof data === "string") {
+  console.log(data.toUpperCase());
+} else if (Array.isArray(data)) {
+  console.log(data.join(", "));
+} else {
+  console.log("kesalahan tipe data");
+}
