0d518c02138102d9f2e7b99e5e2f685c3694bafa
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-24 19:35:34 +0700

    module-9: default parameter

--- module-9/build/js/main.js
+++ module-9/build/js/main.js
@@ -2,9 +2,14 @@
 // function
 function greet(name, age) {
     if (age) {
-        return `Hello ${name}, your age is ${age} years old`;
+        return `Hello ${name}, your age is ${age} years old.`;
     }
     return `Hello ${name}`;
 }
 console.log(greet("Skylar"));
 console.log(greet("Franco", 45));
+function login(firstName, lastName = "Doe") {
+    console.log(`Welcome ${firstName} ${lastName}`);
+}
+login("Gustio");
+login("Hatmaja", "Narotama");
