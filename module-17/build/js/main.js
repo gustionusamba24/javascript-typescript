1a6758ab538e31dbcd38c39bce2faa679bc108c6
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-25 15:16:55 +0700

    module-17: interface

---
+++ module-17/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const user1 = {
+  name: "Nunez",
+  age: 25,
+  isAdmin: false,
+};
+console.log(user1);
---
+++ module-17/src/main.ts
@@ -0,0 +1,13 @@
+interface User {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+}
+
+const user1: User = {
+  name: "Nunez",
+  age: 25,
+  isAdmin: false,
+};
+
+console.log(user1);
