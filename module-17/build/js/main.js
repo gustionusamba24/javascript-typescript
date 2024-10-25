ed7001aad2056b59cc7a8a208581e5099ff8820c
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-25 20:00:14 +0700

    module-17: interface inheritance

--- module-17/build/js/main.js
+++ module-17/build/js/main.js
@@ -1,21 +1,12 @@
 "use strict";
-// const user1: User = {
-//   name: "Nunez",
-//   age: 25,
-//   isAdmin: false,
-// };
-// const user2: User = {
-//   name: "Rapinha",
-//   age: 27,
-// };
-// console.log(user1);
-// console.log(user2);
-const user3 = {
-    name: "Moh. Salah",
+// interface User {
+//   name: string;
+//   age: number;
+//   isAdmin?: boolean;
+//   greet(): string;
+// }
+const employee1 = {
+    name: "Jane Doe",
     age: 32,
-    greet() {
-        return `Hello my name is ${this.name}, I am ${this.age} years old`;
-    },
+    employeeId: 111111,
 };
-console.log(user3);
-console.log(user3.greet());
--- module-17/src/main.ts
+++ module-17/src/main.ts
@@ -1,9 +1,13 @@
-interface User {
-  name: string;
-  age: number;
-  isAdmin?: boolean;
-  greet(): string;
-}
+// interface User {
+//   name: string;
+//   age: number;
+//   isAdmin?: boolean;
+//   greet(): string;
+// }
+
+// interface User {
+//   status?: boolean;
+// }
 
 // const user1: User = {
 //   name: "Nunez",
@@ -19,13 +23,28 @@ interface User {
 // console.log(user1);
 // console.log(user2);
 
-const user3: User = {
-  name: "Moh. Salah",
-  age: 32,
-  greet(): string {
-    return `Hello my name is ${this.name}, I am ${this.age} years old`;
-  },
-};
+// const user3: User = {
+//   name: "Moh. Salah",
+//   age: 32,
+//   greet(): string {
+//     return `Hello my name is ${this.name}, I am ${this.age} years old`;
+//   },
+// };
 
-console.log(user3);
-console.log(user3.greet());
+// console.log(user3);
+// console.log(user3.greet());
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface Employee extends Person {
+  employeeId: number;
+}
+
+const employee1: Employee = {
+  name: "Jane Doe",
+  age: 32,
+  employeeId: 111111,
+};
