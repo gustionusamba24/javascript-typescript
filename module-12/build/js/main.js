153b73597b1dab24e26943928c63c5c58e8b504a
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-25 09:47:01 +0700

    module-12: inheritance

--- module-12/build/js/main.js
+++ module-12/build/js/main.js
@@ -8,9 +8,23 @@ class Product {
         console.log(`Product: ${this.name}, Price: ${this.price}`);
     }
 }
-const product1 = new Product("Laptop", 20000000);
-const product2 = new Product("Handphone", 8000000);
-console.log(product1);
-console.log(product2);
-product1.displayProduct();
-product2.displayProduct();
+class Electronic extends Product {
+    constructor(name, price, warranty) {
+        super(name, price);
+        this.warranty = warranty;
+    }
+    displayElectronic() {
+        super.displayProduct();
+        console.log(`Warranty: ${this.warranty} years`);
+    }
+}
+const electronic1 = new Electronic("Smartphone", 4000000, 2);
+console.log(electronic1);
+electronic1.displayElectronic();
+// electronic1.displayProduct();
+// const product1 = new Product("Laptop", 20000000);
+// const product2 = new Product("Handphone", 8000000);
+// console.log(product1);
+// console.log(product2);
+// product1.displayProduct();
+// product2.displayProduct();
--- module-12/src/main.ts
+++ module-12/src/main.ts
@@ -12,10 +12,30 @@ class Product {
   }
 }
 
-const product1 = new Product("Laptop", 20000000);
-const product2 = new Product("Handphone", 8000000);
+class Electronic extends Product {
+  warranty: number;
 
-console.log(product1);
-console.log(product2);
-product1.displayProduct();
-product2.displayProduct();
+  constructor(name: string, price: number, warranty: number) {
+    super(name, price);
+    this.warranty = warranty;
+  }
+
+  displayElectronic(): void {
+    super.displayProduct();
+    console.log(`Warranty: ${this.warranty} years`);
+  }
+}
+
+const electronic1 = new Electronic("Smartphone", 4000000, 2);
+
+console.log(electronic1);
+electronic1.displayElectronic();
+// electronic1.displayProduct();
+
+// const product1 = new Product("Laptop", 20000000);
+// const product2 = new Product("Handphone", 8000000);
+
+// console.log(product1);
+// console.log(product2);
+// product1.displayProduct();
+// product2.displayProduct();
