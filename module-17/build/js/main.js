830c2aaa85813d0a0da4d13e50e0a30389d2305e
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-25 18:38:48 +0700

    module-17: method in interface

--- module-17/build/js/main.js
+++ module-17/build/js/main.js
@@ -1,7 +1,21 @@
 "use strict";
-const user1 = {
-  name: "Nunez",
-  age: 25,
-  isAdmin: false,
+// const user1: User = {
+//   name: "Nunez",
+//   age: 25,
+//   isAdmin: false,
+// };
+// const user2: User = {
+//   name: "Rapinha",
+//   age: 27,
+// };
+// console.log(user1);
+// console.log(user2);
+const user3 = {
+    name: "Moh. Salah",
+    age: 32,
+    greet() {
+        return `Hello my name is ${this.name}, I am ${this.age} years old`;
+    },
 };
-console.log(user1);
+console.log(user3);
+console.log(user3.greet());
--- module-17/src/main.ts
+++ module-17/src/main.ts
@@ -1,13 +1,31 @@
 interface User {
   name: string;
   age: number;
-  isAdmin: boolean;
+  isAdmin?: boolean;
+  greet(): string;
 }
 
-const user1: User = {
-  name: "Nunez",
-  age: 25,
-  isAdmin: false,
+// const user1: User = {
+//   name: "Nunez",
+//   age: 25,
+//   isAdmin: false,
+// };
+
+// const user2: User = {
+//   name: "Rapinha",
+//   age: 27,
+// };
+
+// console.log(user1);
+// console.log(user2);
+
+const user3: User = {
+  name: "Moh. Salah",
+  age: 32,
+  greet(): string {
+    return `Hello my name is ${this.name}, I am ${this.age} years old`;
+  },
 };
 
-console.log(user1);
+console.log(user3);
+console.log(user3.greet());
