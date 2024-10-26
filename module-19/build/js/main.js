16bf7cf7c94ca1b2183e37fdbda1eb6542e9b7cc
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-26 11:58:41 +0700

    module-19: generic in interface

--- module-19/build/js/main.js
+++ module-19/build/js/main.js
@@ -2,18 +2,7 @@
 // function identity<T>(value: T): T {
 //   return value;
 // }
-// console.log(identity<string>("Gustio Nusamba"));
-// console.log(identity<number>(24));
-class Box {
-    constructor(content) {
-        this.content = content;
-        this.content = content;
-    }
-    getContent() {
-        return this.content;
-    }
-}
-let numberBox = new Box(123);
-let stringBox = new Box("Hello Box");
-console.log(numberBox.getContent());
-console.log(stringBox.getContent());
+const user1 = {
+    name: "Darwin Nunez",
+    age: 25,
+};
--- module-19/src/main.ts
+++ module-19/src/main.ts
@@ -5,17 +5,29 @@
 // console.log(identity<string>("Gustio Nusamba"));
 // console.log(identity<number>(24));
 
-class Box<T> {
-  constructor(public content: T) {
-    this.content = content;
-  }
+// Generic class
+// class Box<T> {
+//   constructor(public content: T) {
+//     this.content = content;
+//   }
 
-  getContent(): T {
-    return this.content;
-  }
+//   getContent(): T {
+//     return this.content;
+//   }
+// }
+
+// let numberBox = new Box<number>(123);
+// let stringBox = new Box<string>("Hello Box");
+// console.log(numberBox.getContent());
+// console.log(stringBox.getContent());
+
+// Generic Interface
+interface User<T, U> {
+  name: T;
+  age: U;
 }
 
-let numberBox = new Box<number>(123);
-let stringBox = new Box<string>("Hello Box");
-console.log(numberBox.getContent());
-console.log(stringBox.getContent());
+const user1: User<string, number> = {
+  name: "Darwin Nunez",
+  age: 25,
+};
