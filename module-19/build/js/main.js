abb7e30bf2736dce7911c4debd4f0759257209d5
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-26 11:52:45 +0700

    module-19: generic in class

--- module-19/build/js/main.js
+++ module-19/build/js/main.js
@@ -1,6 +1,19 @@
 "use strict";
-function identity(value) {
-    return value;
+// function identity<T>(value: T): T {
+//   return value;
+// }
+// console.log(identity<string>("Gustio Nusamba"));
+// console.log(identity<number>(24));
+class Box {
+    constructor(content) {
+        this.content = content;
+        this.content = content;
+    }
+    getContent() {
+        return this.content;
+    }
 }
-console.log(identity("Gustio Nusamba"));
-console.log(identity(24));
+let numberBox = new Box(123);
+let stringBox = new Box("Hello Box");
+console.log(numberBox.getContent());
+console.log(stringBox.getContent());
--- module-19/src/main.ts
+++ module-19/src/main.ts
@@ -1,6 +1,21 @@
-function identity<T>(value: T): T {
-  return value;
+// function identity<T>(value: T): T {
+//   return value;
+// }
+
+// console.log(identity<string>("Gustio Nusamba"));
+// console.log(identity<number>(24));
+
+class Box<T> {
+  constructor(public content: T) {
+    this.content = content;
+  }
+
+  getContent(): T {
+    return this.content;
+  }
 }
 
-console.log(identity<string>("Gustio Nusamba"));
-console.log(identity<number>(24));
+let numberBox = new Box<number>(123);
+let stringBox = new Box<string>("Hello Box");
+console.log(numberBox.getContent());
+console.log(stringBox.getContent());
