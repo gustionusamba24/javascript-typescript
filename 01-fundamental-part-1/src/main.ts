1adc908a2ef24e6804a39e2f65995f7bcfa2460f
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-27 23:15:49 +0700

    feat(01-fundamental-part-1): typescript data types

--- 01-fundamental-part-1/src/main.ts
+++ 01-fundamental-part-1/src/main.ts
@@ -5,3 +5,11 @@ let firstName: string = "Gustio";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let year: number;
+console.log(year);
+console.log(typeof year);
+
+year = 2024;
+console.log(year);
+console.log(typeof year);
