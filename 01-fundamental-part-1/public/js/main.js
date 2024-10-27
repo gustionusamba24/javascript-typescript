4d135299396430d17e9923d4c5ee3c0d28cdb257
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-27 22:50:42 +0700

    feat(01-fundamental-part-1): ts fundamental part 1

---
+++ 01-fundamental-part-1/public/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Samba");
+console.log(24);
+let firstName = "Gustio";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
---
+++ 01-fundamental-part-1/src/main.ts
@@ -0,0 +1,7 @@
+console.log("Samba");
+console.log(24);
+
+let firstName: string = "Gustio";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
