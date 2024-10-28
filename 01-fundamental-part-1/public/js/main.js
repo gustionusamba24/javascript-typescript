ccbb31821d2d758bb64c120f88d90f87e1d19243
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-28 08:30:21 +0700

    feat(01-fundamental-part-1): basic operator and operator precedence

--- 01-fundamental-part-1/public/js/main.js
+++ 01-fundamental-part-1/public/js/main.js
@@ -6,8 +6,15 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 let year;
-console.log(year);
-console.log(typeof year);
+// console.log(year);
+// console.log(typeof year);
 year = 2024;
 console.log(year);
 console.log(typeof year);
+// let, const, var
+// let = block-scoped
+// var = function-scoped
+const currentYear = 2024;
+const ageSamba = currentYear - 2000;
+const ageHatma = currentYear - 2007;
+console.log(ageSamba, ageHatma);
--- 01-fundamental-part-1/src/main.ts
+++ 01-fundamental-part-1/src/main.ts
@@ -7,9 +7,18 @@ console.log(firstName);
 console.log(firstName);
 
 let year: number;
-console.log(year);
-console.log(typeof year);
+// console.log(year);
+// console.log(typeof year);
 
 year = 2024;
 console.log(year);
 console.log(typeof year);
+
+// let, const, var
+// let = block-scoped
+// var = function-scoped
+
+const currentYear: number = 2024;
+const ageSamba: number = currentYear - 2000;
+const ageHatma: number = currentYear - 2007;
+console.log(ageSamba, ageHatma);
