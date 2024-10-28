0ddf76fbdb016010353e18af72d4e2992368bb56
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-10-28 11:20:47 +0700

    feat(01-fundamental-part-1): type conversion and type coercion

--- 01-fundamental-part-1/public/js/main.js
+++ 01-fundamental-part-1/public/js/main.js
@@ -18,3 +18,18 @@ const currentYear = 2024;
 const ageSamba = currentYear - 2000;
 const ageHatma = currentYear - 2007;
 console.log(ageSamba, ageHatma);
+// Conversion and Coercion
+// type conversion
+console.log(Number("Samba"));
+console.log(typeof NaN);
+const numberToString = String(24);
+console.log(typeof numberToString);
+console.log(String(24), 24);
+// type coercion
+console.log("I am " + 24 + " years old");
+console.log("24" - "5" - 9); // convert string to number
+console.log("20" * "5" - 10); // convert string to number
+console.log(Number("24") - Number("5") - 9);
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
--- 01-fundamental-part-1/src/main.ts
+++ 01-fundamental-part-1/src/main.ts
@@ -22,3 +22,23 @@ const currentYear: number = 2024;
 const ageSamba: number = currentYear - 2000;
 const ageHatma: number = currentYear - 2007;
 console.log(ageSamba, ageHatma);
+
+// Conversion and Coercion
+// type conversion
+console.log(Number("Samba"));
+console.log(typeof NaN);
+
+const numberToString: string = String(24);
+console.log(typeof numberToString);
+console.log(String(24), 24);
+
+// type coercion
+console.log("I am " + 24 + " years old");
+console.log("24" - "5" - 9); // convert string to number
+console.log("20" * "5" - 10); // convert string to number
+
+console.log(Number("24") - Number("5") - 9);
+
+let n: any = "1" + 1;
+n = n - 1;
+console.log(n);
