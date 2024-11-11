f4486013ada360e3e7ad3b5ad3c1c41092cf51d0
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-11-11 15:00:10 +0700

    feat(04-ts-data-structures): strings part 1

---
+++ 04-ts-data-structures/src/strings-1.ts
@@ -0,0 +1,33 @@
+const airline = "TAP Air Portugal";
+const plane = "A320";
+
+console.log(airline.length);
+console.log("B737".length);
+
+console.log(airline.indexOf("A"));
+console.log(airline.indexOf("r"));
+console.log(airline.lastIndexOf("r"));
+console.log(airline.indexOf("Air"));
+
+console.log(airline.slice(4, 7));
+console.log(airline.slice(8, 11));
+
+console.log(airline.slice(0, airline.indexOf(" ")));
+console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -3));
+
+const checkMiddleSeat = function (seat: string) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === "B" || s === "E") {
+    console.log("You got the middle seat");
+  } else {
+    console.log("You got lucky");
+  }
+};
+
+checkMiddleSeat("11B");
+checkMiddleSeat("23C");
+checkMiddleSeat("3E");
