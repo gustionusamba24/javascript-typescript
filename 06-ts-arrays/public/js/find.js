1c4f29ca91b6cdf1cd6cd99d528ed57de25c857e
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-11-16 22:36:16 +0700

    feat(06-ts-arrays): find method

---
+++ 06-ts-arrays/public/js/find.js
@@ -0,0 +1,12 @@
+"use strict";
+// find will not return new array, but it will only return the first element in the array that satisfies this condition
+const movements5 = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const firstWithdrawal = movements5.find((mov) => mov < 0);
+console.log(movements5);
+console.log(firstWithdrawal);
+// the distinction between filter and find
+// filter returns all the elements in the array that match with condition (filter method returns new array)
+/*
+find only returns first element in the array (find only returns the element itself and not an array) =>
+the purpose of using find method is simply to find the exactly one element
+ */
