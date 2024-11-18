aedeccc5023d331a51fd5962bde894d2dfdee725
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-11-18 20:37:50 +0700

    feat(06-ts-arrays): array method practice

---
+++ 06-ts-arrays/src/array-method-practice.ts
@@ -0,0 +1,103 @@
+type AccountAP = {
+  owner: string;
+  movements: number[];
+  interestRate: number;
+  pin: number;
+  username?: string;
+  balance?: number;
+};
+
+const account1AP: AccountAP = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2,
+  pin: 1111,
+};
+
+const account2AP = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3AP = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4AP = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accountsAP: AccountAP[] = [
+  account1AP,
+  account2AP,
+  account3AP,
+  account4AP,
+];
+
+// 1. Calculate the total deposits in all accounts
+const bankDepositSum = accountsAP
+  .flatMap((acc: AccountAP): number[] => acc.movements)
+  .filter((mov: number): boolean => mov > 0)
+  .reduce((sum: number, mov: number): number => sum + mov, 0);
+
+console.log(bankDepositSum);
+
+// 2. Count how many deposits are at least $1000
+// method 1
+// const numDeposits1000 = accountsAP
+//   .flatMap((acc: AccountAP): number[] => acc.movements)
+//   .filter((mov: number): boolean => mov >= 1000).length;
+
+// method 2
+const numDeposits1000 = accountsAP
+  .flatMap((acc: AccountAP): number[] => acc.movements)
+  .reduce(
+    (count: number, cur: number): number => (cur >= 1000 ? count + 1 : count),
+    0
+  );
+
+console.log(numDeposits1000);
+
+// 3. create new object with sum of deposits and sum of withdrawals
+const { depositsAP, withdrawalsAP } = accountsAP
+  .flatMap((acc: AccountAP): number[] => acc.movements)
+  .reduce(
+    (
+      sums: { depositsAP: number; withdrawalsAP: number },
+      cur: number
+    ): { depositsAP: number; withdrawalsAP: number } => {
+      sums[cur > 0 ? "depositsAP" : "withdrawalsAP"] += cur;
+      return sums;
+    },
+    { depositsAP: 0, withdrawalsAP: 0 }
+  );
+
+console.log(depositsAP, withdrawalsAP);
+
+// 4. Convert any string to title case
+const convertTitleCase = function (title: string) {
+  const exeptions = ["a", "an", "and", "the", "but", "or", "on", "in", "with"];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(" ")
+    .map((word: string) => {
+      if (exeptions.includes(word)) return word;
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+
+  return titleCase;
+};
+
+console.log(convertTitleCase("this is a nice title"));
+console.log(convertTitleCase("this is a LONG title but not too long"));
+console.log(convertTitleCase("and here is another title with an EXAMPLE"));
