2cf085650b353a2846afd85de5aff33af532246a
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-11-30 12:41:43 +0700

    feat(09-ts-async): consuming promise

--- 09-ts-async/public/js/script.js
+++ 09-ts-async/public/js/script.js
@@ -1,3 +1,34 @@
 "use strict";
 const btn = document.querySelector(".btn-country");
-const container = document.querySelector(".countries");
+const countriesContainer = document.querySelector(".countries");
+// Render country
+function renderCountry(data, className = "") {
+    const language = Object.keys(data.languages);
+    const currency = Object.keys(data.currencies);
+    console.log(language, currency);
+    const html = `
+    <article class="country" ${className}>
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[language[0]]}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[currency[0]].name}</p>
+        </div>
+    </article>
+    `;
+    countriesContainer?.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = "100";
+}
+// Promises and fetch API
+const request = fetch("https://restcountries.com/v3.1/name/indonesia");
+console.log(request);
+function getCountryData(country) {
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+        .then((response) => response.json())
+        .then((data) => renderCountry(data[0]));
+}
+getCountryData("indonesia");
+getCountryData("argentina");
+getCountryData("germany");
--- 09-ts-async/src/script.ts
+++ 09-ts-async/src/script.ts
@@ -1,2 +1,44 @@
 const btn = document.querySelector(".btn-country");
-const container = document.querySelector(".countries");
+const countriesContainer = document.querySelector(".countries");
+
+// Render country
+function renderCountry(data: any, className = "") {
+  const language = Object.keys(data.languages);
+  const currency = Object.keys(data.currencies);
+  console.log(language, currency);
+
+  const html = `
+    <article class="country" ${className}>
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${
+              data.languages[language[0]]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[currency[0]].name
+            }</p>
+        </div>
+    </article>
+    `;
+
+  countriesContainer?.insertAdjacentHTML("beforeend", html);
+  (countriesContainer as HTMLElement).style.opacity = "100";
+}
+
+// Promises and fetch API
+const request = fetch("https://restcountries.com/v3.1/name/indonesia");
+console.log(request);
+
+function getCountryData(country: string): void {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response: Response) => response.json())
+    .then((data: any) => renderCountry(data[0]));
+}
+getCountryData("indonesia");
+getCountryData("argentina");
+getCountryData("germany");
