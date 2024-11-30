527a8b8e45ddc4472d241eea67ffe015e3a75027
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-12-01 00:35:21 +0700

    update ts general

--- ts-general/js/generics.js
+++ ts-general/js/generics.js
@@ -1,17 +1,17 @@
 "use strict";
 const gameScores = [14, 21, 33, 42, 59];
 const favoriteThings = [
-    "raindrops on roses",
-    "whiskers on kittens",
-    "bright copper kettles",
-    "warm woolen mittens",
+  "raindrops on roses",
+  "whiskers on kittens",
+  "bright copper kettles",
+  "warm woolen mittens",
 ];
 const voters = [
-    { name: "Ixia", age: 22 },
-    { name: "Clint", age: 27 },
+  { name: "Ixia", age: 22 },
+  { name: "Clint", age: 27 },
 ];
 function getLastItem(array) {
-    return array[array.length - 1];
+  return array[array.length - 1];
 }
 console.log(getLastItem(gameScores));
 console.log(getLastItem(favoriteThings));
